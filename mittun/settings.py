f0aea68134584a1ede21f00d1205982a123c715f
Francisco Souza <francisco@franciscosouza.net>
2011-04-01 13:46:29 -0300

    Added south

--- mittun/settings.py
+++ mittun/settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'events',
+    'south',
 )
 
 TEST_RUNNER = 'specloud_runner.SpecloudTestRunner'
