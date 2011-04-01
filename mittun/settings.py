ad2b82ee770eb6efecaaf0134ac75b7ef045197b
Francisco Souza <francisco@franciscosouza.net>
2011-03-31 23:07:23 -0300

    Installed event app

--- mittun/settings.py
+++ mittun/settings.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'events',
 )
 
 TEST_RUNNER = 'specloud_runner.SpecloudTestRunner'
