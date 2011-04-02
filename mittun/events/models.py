2155143928ec979ccd5fcdf0e4cec420d0b9955b
Flavia Missi <flaviamissi@gmail.com>
2011-04-02 12:09:33 -0300

    Changed in Event model

--- mittun/events/models.py
+++ mittun/events/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 class Event(models.Model):
+    name = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
+    url = models.CharField(max_length=250)
     date = models.DateField()
     location = models.CharField(max_length=100)
     address = models.CharField(max_length=250)
+
+    def __unicode__(self):
+        return self.name
