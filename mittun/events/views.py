cf68541ed0ca08010b79317da57e4458306c4196
Flavia Missi <flaviamissi@gmail.com>
2011-04-02 13:00:39 -0300

    Added index and event actions

--- mittun/events/views.py
+++ mittun/events/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from models import Event
+
+def index(request):
+    events = Event.objects.all()
+    return render_to_response('index.html', {'events' : events})
+
+def event(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+    return render_to_response('event.html', {'event' : event})
