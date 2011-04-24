3e051da263ed3bb4713bab170657ad521a7e0e7a
Flavia Missi <flaviamissi@gmail.com>
2011-04-24 11:16:00 -0300

    Importing model Event with all modules path

--- mittun/events/views.py
+++ mittun/events/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from models import Event
+from mittun.events.models import Event
 
 def index(request):
     events = Event.objects.all()
