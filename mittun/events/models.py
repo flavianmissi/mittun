f4fde77a9aab9186c9300d834a091b103ddf4064
Flavia Missi <flaviamissi@gmail.com>
2011-04-23 23:00:31 -0300

    Made the url attribute optional

--- mittun/events/models.py
+++ mittun/events/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Event(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
-    url = models.CharField(max_length=250)
+    url = models.CharField(max_length=250, blank=True)
     date = models.DateField()
     location = models.CharField(max_length=100)
     address = models.CharField(max_length=250)
