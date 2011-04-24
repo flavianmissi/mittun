c0a18ef6930a0b4c00a930f8e25e6724b8fd55d0
Flavia Missi <flaviamissi@gmail.com>
2011-04-23 23:01:09 -0300

    Testing the Event model ;)

--- mittun/events/tests/unit/test_event.py
+++ mittun/events/tests/unit/test_event.py
@@ -1,6 +1,26 @@
 import unittest
-from events.models import Event
-from nose.tools import *
+from datetime import date
+from django.db import IntegrityError
+from mittun.events.models import Event
+from nose.tools import raises, assert_equals
 
 class EventTestCase(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.event = Event.objects.create(name="Evento teste",date=date.today())
+
+    def test_should_save_successfully_an_valid_event(self):
+        event = Event(name="", description="Teste", url="none.com", date=date.today(), location="Home", address="Whatever Street, 1542")
+        event.save()
+
+    @raises(IntegrityError)
+    def test_should_not_save_with_an_invalid_event(self):
+        event = Event(name="", description="Teste", url="none.com", location="Home", address="Whatever Street, 1542")
+        event.save()
+
+    @raises(IntegrityError)
+    def test_should_not_save_without_a_name(self):
+        event = Event(name=None, description="Teste", date=date.today(), location="Home", address="Whatever Street, 1542")
+        event.save()
+
+    def test_should_return_the_name_of_the_event_when_call_an_event_directly(self):
+        assert_equals(self.event.__unicode__(), "Evento teste")
