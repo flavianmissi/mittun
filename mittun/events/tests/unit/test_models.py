2c9b7904709a7be20e3b7eee2e511c6fabe917e3
Flavia Missi <flaviamissi@gmail.com>
2011-04-24 11:15:22 -0300

    Renamed model test file

--- mittun/events/tests/unit/test_models.py
+++ mittun/events/tests/unit/test_models.py
@@ -4,16 +4,15 @@ from django.db import IntegrityError
 from mittun.events.models import Event
 from nose.tools import raises, assert_equals
 
-class EventTestCase(unittest.TestCase):
+class EventsModelsTestCase(unittest.TestCase):
     def setUp(self):
         self.event = Event.objects.create(name="Evento teste",date=date.today())
 
     def test_should_save_successfully_an_valid_event(self):
-        event = Event(name="", description="Teste", url="none.com", date=date.today(), location="Home", address="Whatever Street, 1542")
-        event.save()
+        event = Event.objects.create(name="", description="Teste", url="none.com", date=date.today(), location="Home", address="Whatever Street, 1542")
 
     @raises(IntegrityError)
-    def test_should_not_save_with_an_invalid_event(self):
+    def test_should_not_save_without_an_event_date(self):
         event = Event(name="", description="Teste", url="none.com", location="Home", address="Whatever Street, 1542")
         event.save()
 
@@ -23,4 +22,4 @@ class EventTestCase(unittest.TestCase):
         event.save()
 
     def test_should_return_the_name_of_the_event_when_call_an_event_directly(self):
-        assert_equals(self.event.__unicode__(), "Evento teste")
+        assert_equals(self.event.__unicode__(), self.event.name)
