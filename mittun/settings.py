f3fce5242460dd6eff2a0a50e515af33d88ccffc
Flavia Missi <flaviamissi@gmail.com>
2011-05-15 22:26:31 -0300

    Changed app name in settings. This was breaking my testsgit add . o0

--- mittun/settings.py
+++ mittun/settings.py
@@ -123,7 +123,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'events',
+    'mittun.events',
     'south',
 )
 
