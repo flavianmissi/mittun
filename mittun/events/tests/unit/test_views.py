06c5add37eb4638f9fdf677d83bd8e803d8eddb0
Flavia Missi <flaviamissi@gmail.com>
2011-05-15 23:06:49 -0300

    Added missing slash

--- mittun/events/tests/unit/test_views.py
+++ mittun/events/tests/unit/test_views.py
@@ -5,9 +5,9 @@ class EventsViewsTestCase(TestCase):
     fixtures = ['events.json']
 
     def test_should_get_index_and_be_success(self):
-        response = self.client.get('index')
+        response = self.client.get('/index')
         assert_equals(response.status_code, 200)
 
     def test_should_get_an_inexistent_event_and_get_an_404_status(self):
-        response = self.client.get('event/11359')
+        response = self.client.get('/events/11359')
         assert_equals(response.status_code, 404)
