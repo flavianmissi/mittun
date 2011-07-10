0ac46a7cfa26188b590ce049e3a31bf2936c8a87
Flavia Missi <flaviamissi@gmail.com>
2011-07-10 16:22:20 -0300

    Added static files

--- mittun/urls.py
+++ mittun/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -17,3 +18,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
