d45d5f93aee1cc5c5b0006dc7aa1f3a682ce1b1b
Flavia Missi <flaviamissi@gmail.com>
2012-02-05 22:45:33 -0200

    Added admin, index and about views in urls.py

--- mittun/urls.py
+++ mittun/urls.py
@@ -1,13 +1,17 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib import admin
 
-from events.views import IndexView
+from mittun.views import IndexView, AboutView
+
+admin.autodiscover()
 
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view()),
     url(r'^index/?$', IndexView.as_view(), name="index"),
-    url(r'^events/', include('mittun.events.urls')),
+    url(r'^about/(?P<slug>[a-zA-Z-0-9\_]+)/?$', AboutView.as_view(), name="about"),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 urlpatterns += staticfiles_urlpatterns()
