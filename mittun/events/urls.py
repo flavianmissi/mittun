1cddb813f4e057d6dc0f3bac713c959c3bd25067
Flavia Missi <flaviamissi@gmail.com>
2012-02-05 22:46:04 -0200

    removed urls.py from events app

--- mittun/events/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-from events.views import EventsListView, EventDetailView, CreateEvent
-
-urlpatterns = patterns('',
-    url(r'^index/?$', EventsListView.as_view(), name="events_list"),
-    url(r'^details/(?P<slug>[a-zA-Z-0-9\_]+)/?$', EventDetailView.as_view(), name="event_detail"),
-    url(r'^add/?$', CreateEvent.as_view(), name="add_event"),
-)
