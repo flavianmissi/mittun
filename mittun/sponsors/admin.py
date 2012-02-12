a320881ab233cad293f5e54ea8659db1ed33926d
Flavia Missi <flaviamissi@gmail.com>
2012-02-12 21:58:25 -0200

    Added contact as inline in sponsor admin

--- mittun/sponsors/admin.py
+++ mittun/sponsors/admin.py
@@ -3,12 +3,19 @@ from django.contrib import admin
 from sponsors.models import Sponsor, Category, Contact
 
 
+class ContactInline(admin.TabularInline):
+    model = Contact
+
+
 class ContactAdmin(admin.ModelAdmin):
     pass
 
 
 class SponsorAdmin(admin.ModelAdmin):
-    pass
+
+    inlines = [
+        ContactInline,
+    ]
 
 
 class CategoryAdmin(admin.ModelAdmin):
