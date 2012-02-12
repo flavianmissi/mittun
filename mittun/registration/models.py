4546eadc3fe8060ba06cd0e4a3a4a415f86dd908
Francisco Souza <f@souza.cc>
2012-02-12 15:30:03 -0200

    Issue #4: Added the Subscriber model

--- mittun/registration/models.py
+++ mittun/registration/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Subscriber(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    subscribe_date = models.DateTimeField(auto_now_add=True)
---
+++ mittun/registration/tests/test_model_subscriber.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+from django.db import models as django_models
+
+from registration import models
+
+
+class SubscriberModelTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.field_names = models.Subscriber._meta.get_all_field_names()
+
+    def test_should_have_a_name(self):
+        self.assertIn("name", self.field_names)
+
+    def test_name_should_be_a_CharField(self):
+        field = models.Subscriber._meta.get_field_by_name("name")[0]
+        self.assertIsInstance(field, django_models.CharField)
+
+    def test_name_should_have_at_most_255_characters(self):
+        field = models.Subscriber._meta.get_field_by_name("name")[0]
+        self.assertEquals(255, field.max_length)
+
+    def test_should_have_an_email(self):
+        self.assertIn("email", self.field_names)
+
+    def test_email_should_be_a_EmailField(self):
+        field = models.Subscriber._meta.get_field_by_name("email")[0]
+        self.assertIsInstance(field, django_models.EmailField)
+
+    def test_email_should_have_at_most_255_characters(self):
+        field = models.Subscriber._meta.get_field_by_name("email")[0]
+        self.assertEquals(255, field.max_length)
+
+    def test_should_have_a_subscribe_date(self):
+        self.assertIn("subscribe_date", self.field_names)
+
+    def test_subscribe_date_should_be_a_DateTimeField(self):
+        field = models.Subscriber._meta.get_field_by_name("subscribe_date")[0]
+        self.assertIsInstance(field, django_models.DateTimeField)
+
+    def test_subscribe_date_should_be_automatic_defined_on_creation(self):
+        field = models.Subscriber._meta.get_field_by_name("subscribe_date")[0]
+        self.assertTrue(field.auto_now_add)
+        
