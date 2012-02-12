aec1ea97bd20b67ecfda0358c7c29a81dc6dc106
Flavia Missi <flaviamissi@gmail.com>
2012-02-12 17:18:40 -0200

    Added edit action to bootstrap view

--- mittun/events/tests/acceptance/test_event_view.py
+++ mittun/events/tests/acceptance/test_event_view.py
@@ -1,21 +1,24 @@
+from datetime import datetime
 from django.utils.unittest import TestCase
 
 from events.management.browser_helper import browser
 from events.models import Event
 
 
-class EventViewTestCase(TestCase):
+class BaseEventTestCase(object):
 
     def setUp(self):
         self.base_url = "http://localhost:8000"
+        browser.visit('%s/bootstrap' % self.base_url)
+
+
+class EventViewTestCase(BaseEventTestCase, TestCase):
 
     def test_should_visit_the_bootstrap_page_and_be_success(self):
-        browser.visit('%s/bootstrap' % self.base_url)
         self.assertEqual(200, browser.status_code)
 
     def test_should_add_an_event(self):
         events_count = Event.objects.all().count()
-        browser.visit('%s/bootstrap' % self.base_url)
         browser.fill('name', 'a really cool event')
         browser.fill('description', 'this is a really cool event')
         browser.fill('date', '10/02/10')
@@ -24,3 +27,20 @@ class EventViewTestCase(TestCase):
         browser.find_by_name('save').first.click()
         self.assertGreater(Event.objects.all().count(), events_count)
         Event.objects.filter(name='a really cool event').delete()
+
+
+class EditEventTestCase(BaseEventTestCase, TestCase):
+
+    def setUp(self):
+        Event.objects.create(
+            name='some event',
+            description='some description',
+            date=datetime.now(),
+            location='some location',
+            address='some address'
+        )
+        super(EditEventTestCase, self).setUp()
+
+    def test_should_edit_an_event(self):
+        field_name = browser.find_by_name('name').first.value
+        self.assertEqual('some event', field_name)
--- mittun/events/views.py
+++ mittun/events/views.py
@@ -10,3 +10,14 @@ class BootstrapView(CreateView):
     form_class = BootstrapForm
     template_name = 'event_form.html'
     success_url = '/'
+
+    def get(self, request, *args, **kwargs):
+        events = self.model.objects.all()
+        if events.count():
+            self.object = events[0]
+        else:
+            self.object = None
+
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        return self.render_to_response(self.get_context_data(form=form))
