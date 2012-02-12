08e8bd1f1db3b294bc365b5fae83ecee1c94cf1e
Flavia Missi <flaviamissi@gmail.com>
2012-02-12 19:00:01 -0200

    Added admin to category model

--- mittun/sponsors/tests/unit/test_admin.py
+++ mittun/sponsors/tests/unit/test_admin.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 from django.contrib import admin
 
-from sponsors.models import Sponsor
-from sponsors.admin import SponsorAdmin
+from sponsors.models import Sponsor, Category
+from sponsors.admin import SponsorAdmin, CategoryAdmin
 
 
 class AdminTestCase(TestCase):
@@ -12,3 +12,9 @@ class AdminTestCase(TestCase):
 
     def test_should_register_sponsor_model_with_SponsorAdmin_class(self):
         self.assertIsInstance(admin.site._registry[Sponsor], SponsorAdmin)
+
+    def test_should_register_category_model(self):
+        self.assertIn(Category, admin.site._registry)
+
+    def test_should_register_sponsor_model_with_CategoryAdmin_class(self):
+        self.assertIsInstance(admin.site._registry[Category], CategoryAdmin)
