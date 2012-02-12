20576c2fbe7727ae8bff7e21d33da3b9d1452001
Francisco Souza <f@souza.cc>
2012-02-12 19:38:33 -0200

    Added the send_mail view to the SubscriberAdmin class

--- mittun/registration/admin.py
+++ mittun/registration/admin.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.template import response
 
 from registration import models
 
 
 class SubscriberAdmin(admin.ModelAdmin):
-    pass
+
+    def send_mail(self, request):
+        context = {"subscribers": models.Subscriber.objects.all()}
+        return response.TemplateResponse(request, "send_subscribers_mail.html", context)
 
 admin.site.register(models.Subscriber, SubscriberAdmin)
