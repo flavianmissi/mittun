441e94931e675feec88eaf821463e0d53fae19f7
Flavia Missi <flaviamissi@gmail.com>
2012-02-12 22:59:49 -0200

    Created migration to logo field in event model

---
+++ mittun/events/migrations/0008_auto__add_field_event_logo.py
@@ -0,0 +1,41 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding field 'Event.logo'
+        db.add_column('events_event', 'logo', self.gf('django.db.models.fields.files.ImageField')(default=None, max_length=100), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Deleting field 'Event.logo'
+        db.delete_column('events_event', 'logo')
+
+
+    models = {
+        'events.event': {
+            'Meta': {'object_name': 'Event'},
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'description': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'logo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50', 'db_index': 'True'}),
+            'url': ('django.db.models.fields.CharField', [], {'max_length': '250', 'blank': 'True'})
+        },
+        'events.location': {
+            'Meta': {'object_name': 'Location'},
+            'address': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'event': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['events.Event']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '150'})
+        }
+    }
+
+    complete_apps = ['events']
