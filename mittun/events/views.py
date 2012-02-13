fa97ff28cf6565ec3a5bff44502ac3e9b9bd636f
Flavia Missi <flaviamissi@gmail.com>
2012-02-13 00:20:30 -0200

    Fixing create update view for an event

--- mittun/events/views.py
+++ mittun/events/views.py
@@ -1,23 +1,43 @@
-from django.views.generic import CreateView
+from django.views.generic.base import TemplateResponseMixin
+from django.views.generic.edit import ModelFormMixin, ProcessFormView
 
 from events.forms import BootstrapForm
 from events.models import Event
 
 
-class BootstrapView(CreateView):
-
-    model = Event
-    form_class = BootstrapForm
-    template_name = 'event_form.html'
+class BaseCreateUpdateView(ModelFormMixin, ProcessFormView):
 
     def get(self, request, *args, **kwargs):
+        self.set_object()
+        return super(BaseCreateUpdateView, self).get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.set_object()
+        return super(BaseCreateUpdateView, self).post(request, *args, **kwargs)
+
+    def set_object(self):
         events = self.model.objects.all()
+
         if events.count():
             self.object = events[0]
         else:
             self.object = None
 
-        form_class = self.get_form_class()
-        form = self.get_form(form_class)
 
-        return self.render_to_response(self.get_context_data(form=form))
+class BootstrapView(TemplateResponseMixin, BaseCreateUpdateView):
+
+    model = Event
+    form_class = BootstrapForm
+    template_name = 'event_form.html'
+    success_url = '/bootstrap'
+
+#     def get_queryset(self):
+#         return self.model.objects.all()[0]
+
+    # def get(self, request, *args, **kwargs):
+    #     self.set_object()
+
+    #     form_class = self.get_form_class()
+    #     form = self.get_form(form_class)
+
+    #     return self.render_to_response(self.get_context_data(form=form))
