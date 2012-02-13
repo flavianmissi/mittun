db958bf1caeb51cd5389f002585efadd2fd2250b
Francisco Souza <f@souza.cc>
2012-02-12 22:09:32 -0200

    Implemented the method that receives the data via POST and send the email
    The send_mail interface is not implemented yet, neither the mail
    receivers list

--- mittun/registration/admin.py
+++ mittun/registration/admin.py
@@ -6,17 +6,49 @@ from django.template import response
 from registration import forms, models
 
 
+class MailSender(object):
+
+    def send_mail(self, subject, receivers, body):
+        """
+        Simple interface to be mocked.
+
+        TODO:
+
+            - implement the mail sender
+            - move this class to another module
+        """
+        pass
+
+
 class SubscriberAdmin(admin.ModelAdmin):
+
+    def __init__(self, *args, **kwargs):
+        super(SubscriberAdmin, self).__init__(*args, **kwargs)
+        self.mail_sender = MailSender()
 
     def get_urls(self):
         urls = super(SubscriberAdmin, self).get_urls()
         custom_urls = patterns('',
-            (r'^send-mail/$', self.admin_site.admin_view(self.send_mail))
+            (r'^send-mail/$', self.admin_site.admin_view(self.show_mail_form))
         )
         return custom_urls + urls
 
+    def show_mail_form(self, request):
+        if request.method == "POST":
+            return self.send_mail(request)
+
+        context = {"form": forms.SendMailForm()}
+        return response.TemplateResponse(request, "send_subscribers_mail.html", context)
+
     def send_mail(self, request):
-        context = {"subscribers": models.Subscriber.objects.all(), "form": forms.SendMailForm()}
+        form = forms.SendMailForm(request.POST)
+
+        if form.is_valid():
+            self.mail_sender.send_mail(form.data["subject"], [], form.data["body"])
+            context = {"subscribers": models.Subscriber.objects.all()}
+            return response.TemplateResponse(request, "subscribers_mail_sent.html", context)
+
+        context = {"form": form}
         return response.TemplateResponse(request, "send_subscribers_mail.html", context)
 
 admin.site.register(models.Subscriber, SubscriberAdmin)
--- mittun/registration/tests/test_admin_subscriber.py
+++ mittun/registration/tests/test_admin_subscriber.py
@@ -21,31 +21,89 @@ class AdminSubscriberSendMailViewTestCase(test.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        management.call_command("loaddata", "subscribers.yaml", verbosity=0)
-
         factory = client.RequestFactory()
         request = factory.get("/admin/registration/subscriber/send-mail")
 
-        cls.admin = admin.SubscriberAdmin(models.Subscriber.objects.get(pk=1), None)
-        cls.response = cls.admin.send_mail(request)
-
-    @classmethod
-    def tearDownClass(cls):
-        management.call_command("flush", interactive=False, verbosity=0)
+        cls.admin = admin.SubscriberAdmin(models.Subscriber(), None)
+        cls.response = cls.admin.show_mail_form(request)
 
     def test_should_have_a_view_to_send_email_to_all_subscribers(self):
-        assert hasattr(self.admin, "send_mail")
+        assert hasattr(self.admin, "show_mail_form")
 
-    def test_send_mail_view_should_return_a_TemplateResponse(self):
+    def test_show_mail_form_view_should_return_a_TemplateResponse(self):
         self.assertIsInstance(self.response, response.TemplateResponse)
 
-    def test_send_mail_view_should_render_the_send_subscribers_email(self):
+    def test_show_mail_form_view_should_render_the_send_subscribers_email(self):
         self.assertEquals("send_subscribers_mail.html", self.response.template_name)
-
-    def test_should_render_the_template_with_all_subscribers_in_the_context(self):
-        context = self.response.context_data
-        self.assertEquals(list(models.Subscriber.objects.all()), list(context["subscribers"]))
 
     def test_should_include_a_SendMailForm_instance_in_the_context(self):
         context = self.response.context_data
         self.assertIsInstance(context["form"], forms.SendMailForm)
+
+
+class AdminSubscriberSendMailPostInvalidDataTestCase(test.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.data = {
+            "subject": "Welcome to the Django",
+            "body": "",
+        }
+
+        factory = client.RequestFactory()
+        request = factory.post("/admin/registration/subscriber/send-mail", cls.data)
+        cls.admin = admin.SubscriberAdmin(models.Subscriber(), None)
+        cls.response = cls.admin.show_mail_form(request)
+
+    def test_should_return_a_TemplateResponse_instance(self):
+        self.assertIsInstance(self.response, response.TemplateResponse)
+
+    def test_should_render_the_same_template(self):
+        self.assertEqual("send_subscribers_mail.html", self.response.template_name)
+
+    def test_should_include_the_form_filled_up_with_the_fields_contents(self):
+        form = self.response.context_data["form"]
+        key_function = lambda x: x[0]
+        expected = sorted(self.data.items(), key=key_function)
+        got = sorted(form.data.items(), key=key_function)
+        self.assertEquals(expected, got)
+
+
+class AdminSubscriberSendMailWithValidData(test.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        management.call_command("loaddata", "subscribers.yaml", verbosity=0)
+
+        cls.data = {
+            "subject": "Welcome to the Django",
+            "body": "Now die",
+        }
+        cls.sent = False
+
+        def send(subject, receivers, body):
+            cls.sent = True
+
+        factory = client.RequestFactory()
+        request = factory.post("/admin/registration/subscriber/send-mail", cls.data)
+        cls.admin = admin.SubscriberAdmin(models.Subscriber.objects.get(pk=1), None)
+        cls.admin.mail_sender.send_mail = send
+        cls.response = cls.admin.show_mail_form(request)
+
+    @classmethod
+    def tearDownClass(cls):
+        management.call_command("flush", verbosity=0, interactive=False)
+
+    def test_should_send_the_mail_using_the_mail_sender(self):
+        self.assertTrue(self.sent)
+
+    def test_should_return_a_TemplateResponse_instance(self):
+        self.assertIsInstance(self.response, response.TemplateResponse)
+
+    def test_should_render_the_subscriber_mail_sent_template(self):
+        self.assertEqual("subscribers_mail_sent.html", self.response.template_name)
+
+    def test_should_include_all_subscribers_in_the_context(self):
+        expected = list(models.Subscriber.objects.all())
+        got = list(self.response.context_data["subscribers"])
+        self.assertEqual(expected, got)
