87acfda902dbb694916e72bc6610e8a6e8660581
Flavia Missi <flaviamissi@gmail.com>
2012-03-01 21:38:19 -0300

    Added forgotten dependence

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(name="mittun",
       author_email="flaviamissi@gmail.com",
       packages=find_packages(exclude=['docs', '*.tests', '*.tests.*', 'tests.*']),
       include_package_data=True,
-      install_requires=["MySQL-Python==1.2.3", "django==1.3.1", "South==0.7.3", "PIL==1.1.7"],
+      install_requires=["MySQL-Python==1.2.3", "django==1.3.1", "South==0.7.3",
+                        "PIL==1.1.7", "-e git+git://github.com/vbmendes/django-transmeta.git#egg=django-transmeta"],
       tests_require=["nose==1.0.0", "django_nose==0.1.3", "coverage==3.4", "specloud==0.4.2", "splinter", "PyYAML"]
 )
