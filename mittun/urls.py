3ec19d48c9be2c2fd2629e16b4774c83acf3f9e2
Flavia Missi <flaviamissi@gmail.com>
2012-03-17 17:29:13 -0300

    Removed bootstrap url

--- mittun/urls.py
+++ mittun/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
     url(r'^index/?$', IndexView.as_view(), name="index"),
     url(r'^about/?$', AboutView.as_view(), name="about"),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^bootstrap/?$', include('mittun.events.urls'))
+    url(r'^sponsors/?$', include('mittun.sponsors.urls')),
 )
 
 urlpatterns += staticfiles_urlpatterns()
