fb346b48e8e255ce7a24d62871732cc04aedd2ab
Flavia Missi <flaviamissi@gmail.com>
2012-03-17 17:31:48 -0300

    Removing events/urls since its not used anymore

--- mittun/events/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import patterns, url
-
-# from mittun.events.views import BootstrapView
-
-
-urlpatterns = patterns('',
-    #url(r'^$', BootstrapView.as_view(), name='bootstrap'),
-)
