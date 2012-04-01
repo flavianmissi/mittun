54bac5fc33326a3c3a7f3bbaf569b4ec989beb5b
Flavia Missi <flaviamissi@gmail.com>
2012-04-01 12:11:32 -0300

    Sorting imports in sponsors models

--- mittun/sponsors/models.py
+++ mittun/sponsors/models.py
@@ -1,8 +1,8 @@
-from django.db import models
-from django.core.validators import MaxLengthValidator
 from django.contrib.auth.models import User, Permission
-from django.dispatch import receiver
+from django.core.validators import MaxLengthValidator
+from django.db import models
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 from transmeta import TransMeta
 
