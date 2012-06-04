8e5b1e09ac6c105efdc3c0ba58a62ebf7c31b4a2
Francisco Souza <f@souza.cc>
2012-06-04 03:30:41 -0300

    registration: i18n for field names

--- mittun/registration/models.py
+++ mittun/registration/models.py
@@ -1,10 +1,12 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 class Subscriber(models.Model):
-    name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255, unique=True)
-    subscription_date = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=255, verbose_name=_("Name"))
+    email = models.EmailField(max_length=255, unique=True, verbose_name=_("Email"))
+    subscription_date = models.DateTimeField(auto_now_add=True, verbose_name=_("Subscription date"))
 
     def __unicode__(self):
         return self.name
